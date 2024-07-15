b22541622b00f45a11a0ac482ecd84db3ea62bd7
BotJace <147947931+BotJace@users.noreply.github.com>
2024-07-14 22:49:07 -0500

    Move crime_data_LA content to losangeles-data-cleaning folder

---
+++ losangeles-data-cleaning/cleaned_crime_data2022.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Load the dataset
+file_path = r'C:\Users\shrad\OneDrive\Desktop\LA\cleaned_crime_data.csv'
+data = pd.read_csv(file_path)
+
+# Convert the 'date' column to datetime format
+data['date'] = pd.to_datetime(data['date'])
+
+# Filter data for the year 2022
+data_2022 = data[data['date'].dt.year == 2022]
+
+# Save the filtered data to a new CSV file
+output_file_path = r'C:\Users\shrad\OneDrive\Desktop\LA\crime_data_2022.csv'
+data_2022.to_csv(output_file_path, index=False)
+
+print(f"Filtered data for the year 2022 has been saved to {output_file_path}")
